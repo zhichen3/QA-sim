884d7029937381d91dd35d28a37e8def72116b0a
Zhi <zhichen0428@gmail.com>
2021-06-05 18:02:39 -0400

    some bug fix and update

--- quasim/quatel.py
+++ quasim/quatel.py
@@ -22,28 +22,40 @@ class QuaTel:
         self.THETA = (np.pi/2.0)-self.DEC     # convert DEC to THETA
         self.Omega_E = 7.292*10.0**(-5.0)     # Earth Rotation speed (+z) [rad/sec]
         
-    def get_w_f(self, pos, lam):
-        # pos enter in 2-D array [[RA1,DEC1],[RA2,DEC2]] in rad
+    def get_num_photon(self, pos, lam, s1, s2, T, type_xy):
+        # pos enter in 2-D list [[RA1,DEC1],[RA2,DEC2]] in rad
         # lambda in meter
-  
+        # s1 and s2 are the spectral flux density of two sources
+        # type_xy: enter 'pos' for cg,dh and 'neg' for ch,dg
+
         pos = np.array(pos)
 
-        if (np.absolute(pos[:,1]-self.DEC) >= (np.pi/2.)).all():
+        if (np.absolute(pos[:,1]-self.DEC) >= (np.pi/2.)).any():
 
-            raise ValueError("Sources out of reach")
+            raise ValueError("At least 1 source out of reach")
 
         else:
             
             #change position of sources from declination to theta
             pos[:,1] = (np.pi/2.0)-pos[:,1]
 
-        def source_pos(pos,ti):
-            # enter time in sec and pos in 2-D array
+        def source_pos(posi,ti):
+            # enter time (1D Array) in sec and posi in 2-D array
+            # find source position in tele coord
+
+            N = ti.size
 
             #change from spherical to cartesian for easier rotation:
-            x = np.sin(pos[:,1])*np.cos(pos[:,0]+self.Omega_E*ti)
-            y = np.sin(pos[:,1])*np.sin(pos[:,0]+self.Omega_E*ti)
-            z = np.cos(pos[:,1])
+            x1 = np.sin(posi[0,1])*np.cos(posi[0,0]+self.Omega_E*ti)  #x,y for source 1
+            y1 = np.sin(posi[0,1])*np.sin(posi[0,0]+self.Omega_E*ti)
+
+            x2 = np.sin(posi[1,1])*np.cos(posi[1,0]+self.Omega_E*ti)  #x,y for source 2
+            y2 = np.sin(posi[1,1])*np.sin(posi[1,0]+self.Omega_E*ti)
+
+            x = np.vstack((x1,x2))   #[[x1t1,x1t2,...],[x2t1,x2t2,...]]
+            y = np.vstack((y1,y2))
+            
+            z = np.transpose(np.tile(np.cos(posi[0,1]),(N,1)))   #[[z1,z1,...],[z2,z2,...]]
 
             # rotate sources to telescope coord around z then y:
         
@@ -54,66 +66,80 @@ class QuaTel:
             z_new = -np.sin(-self.THETA)*x_new1 + np.cos(-self.THETA)*z
             
             # THETA in tele coord: theta1 = pos[0,1], theta2 = pos[1,1]
-            
-            pos[:,1] = np.arccos(z_new)                # Give THETA
-            pos[:,0] = np.arctan2(y_new,x_new)         # Give PHI
+            new_posi = np.zeros((2,N,2))
+            new_posi[:,:,1] = np.arccos(z_new)                # Give THETA
+            new_posi[:,:,0] = np.arctan2(y_new,x_new)         # Give PHI
 
 
-            return pos
+            return new_posi
 
-
-        # Find Change in THETA in new coord in 1sec:
-
-        Omega_E_new = abs(source_pos(pos,1)[0,1] - source_pos(pos,2)[0,1])
-        
 
         # Find delta_THETA
-        D_THETA = source_pos(pos,0)[0,1] - source_pos(pos,0)[1,1]
+        D_THETA = source_pos(pos,np.array([0.]))[0,0,1] - source_pos(pos,np.array([0.]))[1,0,1]
 
         #Let the source with smaller THETA be THETA_0
         if (D_THETA >= 0.0):
-            THETA_0 = source_pos(pos,0)[1,1]
+            THETA_0 = source_pos(pos,np.array([0.]))[1,0,1]
         else:
-            THETA_0 = source_pos(pos,0)[0,1]
-
+            THETA_0 = source_pos(pos,np.array([0.]))[0,0,1]
 
         D_THETA = abs(D_THETA)
 
-        #Find w_f, fringe rate
+        # Find Omega_E in new coord that depends on time:
+        Omega_E_sam = np.absolute(source_pos(pos,np.array([1.]))[0,0,1] - source_pos(pos,np.array([2.]))[0,0,1])
+
+        #Find w_f_sam, sample fringe rate used to approx time interval
         if (D_THETA < 0.1):
-            w_f = (2.0*np.pi*self.B*Omega_E_new*np.sin(THETA_0)*D_THETA)/lam
+            w_f_sam = (2.0*np.pi*self.B*Omega_E_sam*np.sin(THETA_0)*D_THETA)/lam
+        else:
+            w_f_sam= (2.0*np.pi*self.B*Omega_E_sam/lam)*(np.sin(THETA_0)*
+                    np.sin(D_THETA)+np.cos(THETA_0)*(1-np.cos(D_THETA)))
+
+        #approximate time interval then break up period into small time intervals
+        #assume dt << 1/w_f
+        dt = 0.08/w_f_sam
+
+        t = np.linspace(0.0, T, int(T/dt)) 
+
+        # Recalculate Omega_E and w_f  more accurately since it depends on time, only changes slightly:
+        pos_t1 = source_pos(pos,t)
+        pos_t2 = source_pos(pos,t+dt)
+        Omega_E_new =(np.absolute(pos_t1[0,:,1] - pos_t2[0,:,1]))/dt
+
+        if (D_THETA < 0.1): 
+            w_f = (2.0*np.pi*self.B*Omega_E_new*np.sin(THETA_0)*D_THETA)/lam   #w_f: 1-D with N-ele
         else:
             w_f = (2.0*np.pi*self.B*Omega_E_new/lam)*(np.sin(THETA_0)*
                     np.sin(D_THETA)+np.cos(THETA_0)*(1-np.cos(D_THETA)))
+            
+    
+        #redefine dt
+        dt = 0.08/w_f
 
-        return w_f
-
-    def get_num_photon(self, w_f, lam, s1, s2, t, type_xy):
-        # find w_f use func above
-        # assume length of integration is small compared to fringe period
-        # dt << 1/w_f , let dt = 0.01(1/w_f)
-        # s1 and s2 are the spectral flux density of two sources
-        # type_xy: enter 'pos' for cg,dh and 'neg' for ch,dg
-
-        dt = 0.01/w_f
+        #Let dt = 0 when one of the sources abs(THETA) go beyond 90deg, so N_xy = 0.
+        #Not sure if this feature is working properly
+        cond1 = np.where(np.absolute(pos_t1[0,:,1]) > (np.pi/2.0))
+        cond2 = np.where(np.absolute(pos_t1[1,:,1]) > (np.pi/2.0))
+        dt[cond1] = 0.0
+        dt[cond2] = 0.0
         
         k_const = self.tau*10**(-9)*dt*(self.A*self.BW*10**(9)*lam/constants.h/constants.c)**2
 
-        vis = (2.0*s1*s2)/(s1+s2)**2
+        vis = (2.0*s1*s2)/((s1+s2)**2)
         
         N_xy = 1.0/8.0*k_const*(s1+s2)**2
-
+        
         #phase term due to instrumental length diff
         ph = 2.0*np.pi*self.DL/lam
 
 
         if (type_xy == 'pos'):
 
-            return N_xy*(1+vis*np.cos(w_f*t+ph))
+            return N_xy*(1+vis*np.cos(w_f*t+ph)), t
         
         elif (type_xy == 'neg'):             
 
-            return N_xy*(1-vis*np.cos(w_f*t+ph))
+            return N_xy*(1-vis*np.cos(w_f*t+ph)), t
 
         
   #  def precision(self)
